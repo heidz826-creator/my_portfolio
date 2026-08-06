94288289382f24129fe4280d94eb591725a5ca15
Heide Parado <heide.parado26@gmail.com>
2026-08-06 11:30:08 +0800

    Live site pages

--- script.js
+++ script.js
@@ -92,3 +92,22 @@ contactForm.addEventListener("submit", function (event) {
         console.error(error);
     })
 });  
+
+const observer = new IntersectionObserver((entries) => {
+    entries.forEach((entry) => {
+        if (entry.isIntersecting) {
+            entry.target.classList.add("show");
+        } else {
+            entry.target.classList.remove("show");
+        }
+    });
+}, {
+    threshold: 0.2
+
+});
+
+document.querySelectorAll("section").forEach((section) => {
+    section.classList.add("hidden");
+    observer.observe(section);
+});
+
